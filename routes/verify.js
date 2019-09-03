277c5e3e22e67d470ee8013e40b8257829b842e5
Ravi Prakash Pandey <rvpandey99@gmail.com>
2019-09-03 19:29:44 +0530

    updated verify.js

--- routes/verify.js
+++ routes/verify.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verify = (req,res,next) => {
     const token = req.header('authToken');
-    if (!token) return res.status(401).send('Access Denied.');
+    if (!token) return res.status(401).send('Access Denied. Please log in.');
 
     try {
         const verified = jwt.verify(token, process.env.SECRET);
